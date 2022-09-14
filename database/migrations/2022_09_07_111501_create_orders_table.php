603b172d80dd541a74cafecfdc300f4c13b6065e
ALI <ali@local.ma>
2022-09-14 20:30:25 +0100

    test 2

---
+++ database/migrations/2022_09_07_111501_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();     
+            $table->string('name');
+            $table->string('email');
+            $table->decimal('cost',8,2);
+            $table->string('phone');
+            $table->string('adresse');
+            $table->string('statut');
+            $table->string('city');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
